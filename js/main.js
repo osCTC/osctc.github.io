f73606f7b7ad195b6df890a4ce119fec6998a5f7
Ivan Ilic <me@mrisaacs.org>
2018-02-15 00:39:47 +0100

    Update import
    add bittrex importer
    add Trade ID to table column
    move csvImportInput into modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+    var myModal = document.getElementById("myModal");
+
+    var myModalInstance = new Modal(myModal, {
+        backdrop: 'static',
+        keyboard: false
+    });
+
     var datatable = new DataTable("#myTable", {
         columns: [
             // Sort the second column in ascending order
@@ -9,12 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
         ]
     });
 
-    document.getElementById("import").addEventListener('click', function(){
+    document.getElementById("submitImport").addEventListener('click', function(){
         var inputStream = [];
+        var exchangeImport = document.getElementById("importtype").value;
 
-        console.log(datatable.pagers);
-
-        //$
+        // console.log(datatable.pagers);
 
         // Papa.parse(fileInput.files[0], {
         // 	complete: function(results) {
@@ -42,31 +48,79 @@ document.addEventListener("DOMContentLoaded", function() {
             withCredentials: undefined
         });
 
-        [].forEach.call(results.data, function(row) {
-            let selledCoin = row.Market.split(row["Fee Coin"]);
-            selledCoin = selledCoin[0] || selledCoin[1];
-            inputStream.push([
-                // Type
-                row.Type,
-                // Buy
-                row.Type === "BUY" ? row.Amount - row.Fee : row.Total - row.Fee,
-                // Cur.
-                row["Fee Coin"],
-                // Sell
-                row.Type === "BUY" ? row.Total : row.Amount,
-                // Cur.
-                selledCoin,
-                // Rate
-                row.Price,
-                // Fee
-                row.Fee,
-                // Date
-                row.Date
-            ]);
-        });
+        // Binance Import
+        if(exchangeImport == 1) {
+            datatable.rows().add(binanceImport(results.data));
+            myModalInstance.hide();
+        }
+        // Bittrex Import
+        else if (exchangeImport == 2) {
+            datatable.rows().add(bittrexImport(results.data));
+            myModalInstance.hide();
+        } else {
+            alert("Choose an exchange.");
+        }
 
-        console.log(inputStream);
 
-        datatable.rows().add(inputStream);
     });
 });
+
+function binanceImport(data) {
+    let inputStream = [];
+    [].forEach.call(data, function(row) {
+        let selledCoin = row.Market.split(row["Fee Coin"]);
+        selledCoin = selledCoin[0] || selledCoin[1];
+        inputStream.push([
+            // Type
+            row.Type,
+            // Buy
+            row.Type === "BUY" ? row.Amount - row.Fee : row.Total - row.Fee,
+            // Cur.
+            row["Fee Coin"],
+            // Sell
+            row.Type === "BUY" ? row.Total : row.Amount,
+            // Cur.
+            selledCoin,
+            // Rate
+            row.Price,
+            // Fee
+            row.Fee,
+            // Date
+            row.Date,
+            // Trade ID
+            "-"
+        ]);
+    });
+
+    return inputStream;
+}
+
+function bittrexImport(data) {
+    let inputStream = [];
+    [].forEach.call(data, function(row) {
+        let selledCoin = row.Exchange.split("-");
+        [selledCoin[0], selledCoin[1]] = row.Type === "LIMIT_BUY" ? [selledCoin[1], selledCoin[0]] : [selledCoin[0], selledCoin[1]];
+        inputStream.push([
+            // 1 Type
+            row.Type === "LIMIT_BUY" ? "BUY" : "SELL",
+            // 2 Buy
+            row.Type === "LIMIT_BUY" ? row.Quantity : row.Price - row.CommissionPaid,
+            // 3 Cur.
+            selledCoin[0],
+            // 4 Sell
+            row.Type === "LIMIT_BUY" ? row.Price + row.CommissionPaid : row.Quantity,
+            // 5 Cur.
+            selledCoin[1],
+            // 6 Rate
+            row.Price,
+            // 7 Fee
+            row.CommissionPaid,
+            // 8 Date
+            row.Closed,
+            // 9 Trade ID
+            row.OrderUuid
+        ]);
+    });
+
+    return inputStream;
+}
